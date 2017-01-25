7c356cc256ee6b0b2b85f0eaee7aa9fdfb60fd80
clearlyTHUYDOAN <clearlythuydoan@gmail.com>
2017-01-24 19:30:55 -0500

    build skeleton

---
+++ api-app/components/list-item.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class ListItem extends Component {
+  render() {
+    return (
+      <div className="List-Item">
+        <h2>The ListItem component is working.</h2>
+      </div>
+    );
+  }
+}
+
+export default ListItem;
---
+++ api-app/components/list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class List extends Component {
+  render() {
+    return (
+      <div className="List">
+        <h2>The List component is working.</h2>
+      </div>
+    );
+  }
+}
+
+export default List;
