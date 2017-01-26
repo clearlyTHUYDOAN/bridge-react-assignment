7588c12f648bdc9b5d835f2dbfcde9522ab42717
clearlyTHUYDOAN <clearlythuydoan@gmail.com>
2017-01-25 21:50:34 -0500

    update note for purvi and della

--- api-app/src/App.js
+++ api-app/src/App.js
@@ -2,11 +2,10 @@
 
 // Hey guys, I found I got all jumbled up when I was trying to synthesize React from my memory
 // and React the way it was taught in Bridge. So, instead of staying stuck, I just decided
-// to build this from memory because I hypothesized it'd be easier to "upgrade" my way of coding
+// to build this from memory because I thought maybe it'd be easier to "upgrade" my way of coding
 // in React to Rangle's way after getting it to work. That being said, please tell me where I can 
 // improve the way I code React from my memory based on Rangle's standards of coding in React!
-// I seriously accept all constructive criticism. I love React and would love to specialize
-// eventually if I can. Thanks!
+// I love React and would love to specialize eventually if I can. Thanks!
 
 // You'll also find questions I've had littered throughout. Would love insight into those.
 // i.e. Why can't I sudo npm instlal react-router?
